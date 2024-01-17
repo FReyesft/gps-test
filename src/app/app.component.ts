069aa5eafa3a64b176695ac2eff79ed4b001c2a5
FReyesft <f.reyesft@gmail.com>
2024-01-17 15:35:22 -0500

    add geolocation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'
+import { Geolocation } from '@capacitor/geolocation'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 
-export class AppComponent  {
-  
+export class AppComponent implements OnInit {
+  map: any;
+
+  ngOnInit(): void {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = L.map('map').setView([0, 0], 2)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map)
+  }
 }
--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,7 +1,67 @@
 import { Injectable } from '@angular/core';
+import { LocationAccuracy } from '@ionic-native/location-accuracy';
+import { Capacitor } from '@capacitor/core';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GeolocationService {
 	constructor() { }
-	
+
+	//* Check GPS permissions
+	async checkGPSPermission(): Promise<boolean> {
+		return await new Promise((resolve, reject) => {
+			if (Capacitor.isNativePlatform) {
+				AndroidPermissions.checkPermission(AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION)
+					.then((result) => {
+						if (result.hasPermission) {
+							resolve(true);
+						} else {
+							resolve(false);
+						}
+					}, error => {
+						alert(error)
+					});
+			}
+		});
+	}
+
+	//* Request Permission GPS
+	async requestGPSPermission(): Promise<string> {
+		return await new Promise((resolve, reject) => {
+			LocationAccuracy.canRequest().then((canRequest: boolean) => {
+				if (canRequest) {
+					resolve('CAN_REQUEST');
+				} else {
+					AndroidPermissions.requestPermission(AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION)
+						.then(
+							(result) => {
+								if (result.hasPermission) {
+									resolve('GOT_PERMISSION');
+								} else {
+									reject('DENIED_PERMISSION');
+								}
+							}, error => {
+								alert(`Request permission error requesting location permission ${error}`);
+							}
+						);
+				}
+			});
+		});
+	}
+
+	//* Request to turn on the GPS
+	async askToTurnOnGPS(): Promise<boolean> {
+		return await new Promise((resolve, reject) => {
+			LocationAccuracy.canRequest().then((canRequest: boolean) => {
+				if (canRequest) {
+					LocationAccuracy.request((LocationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY))
+						.then(() => {
+							resolve(true);
+						}, error => { resolve(false) });
+				} else {
+					resolve(false);
+				}
+			});
+		});
+	}
 }
